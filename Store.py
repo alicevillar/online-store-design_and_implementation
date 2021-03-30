266806c7ac87152d424244858fc371a1236311e0
alicevillar <av20678@essx.ac.uk>
2021-03-30 14:39:30 -0300

    Refactoring Code

---
+++ Store.py
@@ -0,0 +1,118 @@
+
+#Creating class Store with 13 lists:
+
+class Store:
+    list_users = []
+    list_warehousestaff = []
+    list_customers = []
+    list_marketplace_vendors = []
+    list_website_Product = []
+    list_marketplaceProducts = []
+    list_shoppingCart = []
+    list_onlineOrders = []
+    list_deliveryCompany = []
+    list_loyaltySchemes = []
+    list_orderDetails = []
+    list_payment = []
+    #list_passwords=[]
+
+#Creating two variables to be used in Part 4 ("Interacting with user")
+    is_logged = False
+    current_user = 0
+
+# Creating 13 functions with methods to add elements in each of the lists above:
+
+    def add_new_user(self, users):
+        users.id_crn = len(self.list_users) + 1
+        self.list_users.append(users)
+
+    def add_new_warehousestaff(self, warehousestaff):
+        warehousestaff.id_crn = len(self.list_warehousestaff) + 1
+        self.list_warehousestaff.append(warehousestaff)
+
+    def add_new_customer(self, customers):
+        customers.id_crn = len(self.list_customers) + 1
+        self.list_customers.append(customers)
+
+    def add_new_marketplace_vendor(self, marketplace):
+        marketplace.id_crn = len(self.list_marketplace_vendors) + 1
+        self.list_marketplace_vendors.append(marketplace)
+
+    def add_new_website_product(self, website_product):
+        website_product.product_id = len(self.list_website_Product) + 1
+        self.list_website_Product.append(website_product)
+
+    def add_new_marketplace_product(self, marketplaceproduct):
+        marketplaceproduct.product_id = len(self.list_marketplaceProducts) + 1
+        self.list_marketplaceProducts.append(marketplaceproduct)
+
+    def add_new_cart(self, cart):
+        cart.shopping_cart_id = len(self.list_shoppingCart) + 1
+        self.list_shoppingCart.append(cart)
+
+    def add_new_onlineOrders(self, online_order):
+        online_order.online_order_id = len(self.list_onlineOrders) + 1
+        self.list_onlineOrders.append(online_order)
+
+    def add_new_delivery_company(self,delivery_company):
+        delivery_company.crn=len(self.list_deliveryCompany) +1
+        self.list_deliveryCompany.append(delivery_company)
+
+    def add_new_loyalty_schemes(self,loyalty_schemes):
+        loyalty_schemes.coupon_id=len(self.list_loyaltySchemes) +1
+        self.list_loyaltySchemes.append(loyalty_schemes)
+
+    def add_new_order_details(self,order_details):
+        order_details.online_order=len(self.list_orderDetails) +1
+        self.list_orderDetails.append(order_details)
+
+    def add_new_payment(self,payment):
+        payment.payment_id=len(self.list_payment) +1
+        self.list_payment.append(payment)
+
+    #def add_new_password(self,passwords):
+        #passwords.password=len(self.list_passwords) +1
+        #self.list_passwords.append(passwords)
+
+
+# Creating 2 methods to list products from the website and products from the marketplace
+
+    def listing_website_Product(self):
+        for p in self.list_website_Product:
+            print(f"Product name: {p.product_name} /n Product ID: {p.product_id}")
+
+    def listing_marketplaceProducts(self):
+        for p in self.list_marketplaceProducts:
+            print(f"Product name: {p.product_name} /n Product ID: {p.product_id}")
+
+# Creating one method to list all products
+
+    def listing_all_products(self):
+        self.listing_website_Product()
+        self.listing_marketplaceProducts()
+
+# Creating one method to check password:
+
+    def check_password(self, user):
+        for u in self.list_users:
+            if user.username==u.username and user.password == u.password:
+            return True
+        else:
+            return False
+            print("Incorrect password :( Try again")
+
+# Checking products availability (return true of false)
+    def cheking_availability_website(self,selected_product):
+        for p in self.list_website_Product:
+            if selected_product.product_id==selected_product:
+            return True
+        else:
+            return False
+
+    def cheking_availability_marketplace(self,selected_product):
+        for p in self.list_marketplaceProducts:
+            if selected_product.product_id==selected_product:
+            return True
+        else:
+            return False
+
